e4de3d749701cf6224cb820c4992a0150df5c1bd
Guilherme Torquato <gtorquato@proton.me>
2023-02-25 16:40:31 -0300

    feat: ssh-agent

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -66,5 +66,12 @@ if status is-interactive
     bind -M insert \cr _atuin_search
 end
 
+# SSH-Agent
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
 #direnv hook fish | source
 
