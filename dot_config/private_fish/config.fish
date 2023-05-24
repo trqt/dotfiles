2d59c7b1223de57e9f53d8112687a19dd094fdf0
Guilherme Torquato <gtorquato@proton.me>
2023-05-24 20:10:53 -0300

    feat: fish revamp
    Exporting more XDG enviroment variables, seatd setup and set `bat` theme
    to catppuccin.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,14 +9,18 @@ set -x XDG_CACHE_HOME $HOME/.cache
 set -x RUSTUP_HOME $XDG_DATA_HOME/rustup  
 set -x CARGO_HOME $XDG_DATA_HOME/cargo
 set -x GOPATH $XDG_DATA_HOME/go
+set -x FLYCTL_INSTALL $XDG_DATA_HOME/fly
 set -x SQLITE_HISTORY $XDG_CACHE_HOME/sqlite_history
 set -x BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundle
 set -x BUNDLE_USER_CACHE $XDG_CACHE_HOME/bundle
 set -x BUNDLE_USER_PLUGIN $XDG_DATA_HOME/bundle
 set -x WINEPREFIX $XDG_DATA_HOME/wine
 set -x LESSHISTFILE $XDG_CACHE_HOME/less_history
+set -x PASSWORD_STORE_DIR $XDG_DATA_HOME/pass
+set -x INPUTRC $XDG_CONFIG_HOME/readline/inputrc 
+set -x _JAVA_OPTIONS "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java" 
 
-set -U fish_user_paths $CARGO_HOME/bin $RUSTUP_HOME/bin $GOPATH/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin ~/.nix-profile/bin $CARGO_HOME/bin $RUSTUP_HOME/bin $GOPATH/bin $FLYCTL_INSTALL/bin $fish_user_paths
 
 # Neovim FTW!
 set -x EDITOR "nvim"
@@ -34,7 +38,7 @@ set -x SDL_VIDEODRIVER wayland
 
 # Misc
 set -x LIBVA_DRIVER_NAME "i965"
-set -x BAT_THEME "gruvbox-dark"
+set -x BAT_THEME "Catppuccin-mocha"
 
 set fish_color_normal brwhite 
 set fish_color_autosuggestion '#7d7d7d' 
@@ -69,6 +73,8 @@ end
 
 set -g fish_key_bindings fish_vi_key_bindings
 
+direnv hook fish | source
+
 if status is-interactive 
     set -gx ATUIN_NOBIND "true"
     atuin init fish | source
@@ -78,12 +84,24 @@ if status is-interactive
     bind -M insert \cr _atuin_search
 end
 
-# SSH-Agent
-if test -z (pgrep ssh-agent | string collect)
-    eval (ssh-agent -c)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+if status is-login
+    # unecessary with elogind
+    if test -z "$XDG_RUNTIME_DIR"
+        set -x XDG_RUNTIME_DIR "/tmp/$UID-runtime-dir"
+        if ! test -d "$XDG_RUNTIME_DIR"
+            mkdir "$XDG_RUNTIME_DIR"
+            chmod 0700 "$XDG_RUNTIME_DIR"
+        end
+    end
+    
+    # SSH-Agent
+    if test -z (pgrep ssh-agent | string collect)
+        eval (ssh-agent -c)
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    end
+    
+    set -x _JAVA_AWT_WM_NONREPARENTING 1
+    dbus-run-session sway
 end
-
-direnv hook fish | source
 
