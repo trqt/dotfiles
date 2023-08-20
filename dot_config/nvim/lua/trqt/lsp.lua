96a9ca2fa81c06af2be09a14c97d25bdf7104d17
Guilherme Torquato <gtorquato@proton.me>
2023-08-20 10:50:38 -0300

    feat(neovim): use manual lsp setup

--- dot_config/nvim/lua/trqt/lsp.lua
+++ dot_config/nvim/lua/trqt/lsp.lua
@@ -1,7 +1,11 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset({
+    setup_servers_on_start = false,
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
+
+lsp.setup_servers({'clangd', 'rust_analyzer', 'gopls', 'lua_ls', 'pyright'})
 
 lsp.setup()
