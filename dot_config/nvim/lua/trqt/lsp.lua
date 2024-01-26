ad1792151bbeea8865ae5b30e7be45df569cc5f9
Guilherme Torquato <gtorquato@proton.me>
2024-01-26 20:12:48 -0300

    feat(neovim): ocaml lsp server!

--- dot_config/nvim/lua/trqt/lsp.lua
+++ dot_config/nvim/lua/trqt/lsp.lua
@@ -6,6 +6,6 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.setup_servers({'clangd', 'rust_analyzer', 'gopls', 'lua_ls', 'pyright'})
+lsp.setup_servers({'clangd', 'rust_analyzer', 'gopls', 'lua_ls', 'pylsp', 'ocamllsp'})
 
 lsp.setup()
